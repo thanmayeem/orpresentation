a4b1c9d0b8246e956160a39e28e8acadea7703d8
thanmayeem <112336813+thanmayeem@users.noreply.github.com>
2022-10-30 16:03:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let leave= document.querySelector('button');
+leave.addEventListener('click',showMsg);
+
+function showMsg(){
+    alert("Have a good day and visit us later");
+}
+
+console.log("hello");
+
